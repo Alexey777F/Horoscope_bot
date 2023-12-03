a68838464812bb5e64d1cd9b5230166f0b21c190
Alexey777F <112414906+Alexey777F@users.noreply.github.com>
2023-12-03 13:04:09 +0300

    cheng start by asyncio

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import logging
-from aiogram.utils import executor
-import handlers
+from handlers import start_handler, zodiakal_handler, chinese_handler, japanese_handler, druid_handler, compatibility_handler, help_handler, echo_handler
 from config.config import dp
-import datetime
-from asyncio.exceptions import TimeoutError
+import asyncio
+import logging
+
+
+async def main():
+    await dp.start_polling()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-while True:
-    logger = logging.getLogger(__name__)
     try:
-        executor.start_polling(dp, skip_updates=True, timeout=20)
-    except TimeoutError as e:
-        logger.error(datetime.datetime.now(), e)
-        continue
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Bot stopped")
+
